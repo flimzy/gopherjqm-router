7649a83f1392470301a3794cd7d6f989016e9df8
Jonathan Hall <flimzy@flimzy.com>
2016-01-06 09:15:44 +0100

    Move jQuery init for tests into router_tests.go, using an ugly hack

--- router_test.go
+++ router_test.go
@@ -9,6 +9,19 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+// This is an ugly hack to force jQuery initialization even earlier than init() would do it
+// so that init() in other files will be run later
+var _ = initjQuery()
+func initjQuery() bool {
+	cwd := js.Global.Get("process").Call("cwd").String()
+	jsdom := js.Global.Call("require", cwd+"/node_modules/jsdom")
+	doc := jsdom.Call("jsdom", "<html></html>")
+	js.Global.Set("window", doc.Get("defaultView"))
+	jQuery := js.Global.Call("require", cwd+"/node_modules/jquery")
+	js.Global.Set("jQuery", jQuery)
+	return true
+}
+
 func TestFoo(t *testing.T) {
 	// Only pass t into top-level Convey calls
 	Convey("Given some integer with a starting value", t, func() {
